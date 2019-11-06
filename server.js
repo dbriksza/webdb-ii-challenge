5d307ff7e217ba0e1c156f3931ed9793e6bea148
dbriksza <dbriksza@gmail.com>
2019-11-06 12:23:54 -0500

    mvp

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const dealerRouter = require("./cars/dealerRouter.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("api/cars", dealerRouter);
+
+module.exports = server;
